5ff87638f62d1794e0136a44a4404d7bb16c788c
Dhana Balan K <113673544+DhanaBalan2001@users.noreply.github.com>
2025-11-13 12:22:08 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: ['https://blog-app-dhanabalan.netlify.app', 'http://localhost:3000'],
+    origin: ['https://blog-app-dhanabalank.netlify.app', 'http://localhost:3000'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS']
 }));
